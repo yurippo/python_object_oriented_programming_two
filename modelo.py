69e9b51b7b8bb11476dfed542638b16fb8de17d8
yuri <yuri.prata12@gmail.com>
2022-08-16 18:01:40 -0700

    My First Mint Commit :)

---
+++ modelo.py
@@ -0,0 +1,24 @@
+class Filme:
+
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+
+
+class Serie:
+
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
+
+
+vingadores = Filme('vingadores - guerra infinita',2018,160)
+
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao}')
+
+atlanta = Serie('atlanta', 2018, 2)
+
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+##new
